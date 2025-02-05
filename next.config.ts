a50acf59f5427cb51e08fdc45781d87f81020600
Paul Laird <plaird83@gmail.com>
2025-02-04 21:45:48 -0500

    Added google analytics

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  env: {
-    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
-  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,21 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import SessionProviderWrapper from "../components/SessionProviderWrapper";
+import Analytics from "@/components/Analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "EZ Minutes",
-  description: "AI-first meeting minutes",
+  description: "Generate your meeting minutes, in minutes!",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Analytics />
+      </head>
       <body className={inter.className}>
         <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
