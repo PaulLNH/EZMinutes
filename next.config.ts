e86e4035eaced93b5591f52208281cf5d7ed9c5c
Paul Laird <plaird83@gmail.com>
2025-02-04 20:59:15 -0500

    Added Google Analitics tracking ID and script to the Next JS App

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  env: {
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
+  },
 };
 
 export default nextConfig;
