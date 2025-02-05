32abe65245deb376c87466055f4b16d76472d431
Paul Laird <plaird83@gmail.com>
2025-02-04 21:52:43 -0500

    Added IDs to the analytics scripts

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -4,15 +4,15 @@ const Analytics = () => {
     const { GA_TRACKING_ID } = process.env;
     return (
         <>
-            <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></Script>
-            <Script>
+            <Script async id="analytics-tag-manager" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></Script>
+            <Script id="analytics-gtag">
                 {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-    
-            gtag('config', '${GA_TRACKING_ID}');
-            `}
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+            
+                    gtag('config', '${GA_TRACKING_ID}');
+                `}
             </Script>
         </>
     );
